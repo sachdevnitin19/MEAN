2c06bdb912aed2f2ad795188d88d72fbcc25a5d5
sachdevnitin19 <sachdevnitin19@gmail.com>
2017-02-08 14:50:54 +0530

    changed db link

--- server.js
+++ server.js
@@ -18,15 +18,7 @@ var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000
                 replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }; 
 
 
-/*mongoose.connect("mongodb://sachdevnitin19:database@ds131729.mlab.com:31729/nvrv",options,function(err){
- 	if(err){
- 		console.log("failed to connect to MongoDB. error:"+err);
-	}
-	else{
-		console.log("connected to MongoDB");
-	}
-});*/
-mongoose.connect("mongodb://localhost:27017/MEAN",options,function(err){
+mongoose.connect("mongodb://sachdevnitin19:database@ds131729.mlab.com:31729/nvrv",options,function(err){
  	if(err){
  		console.log("failed to connect to MongoDB. error:"+err);
 	}
@@ -34,6 +26,14 @@ mongoose.connect("mongodb://localhost:27017/MEAN",options,function(err){
 		console.log("connected to MongoDB");
 	}
 });
+/*mongoose.connect("mongodb://localhost:27017/MEAN",options,function(err){
+ 	if(err){
+ 		console.log("failed to connect to MongoDB. error:"+err);
+	}
+	else{
+		console.log("connected to MongoDB");
+	}
+});*/
 
 app.get('*',function(req,res){
 	res.sendFile(path.join(__dirname+'/public/app/views/index.html'));
