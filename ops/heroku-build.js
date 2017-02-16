b6a01d2dc4b753a1d2d1b7d52dc8ca44190691f0
sachdevnitin19 <sachdevnitin19@gmail.com>
2017-02-16 20:53:54 +0530

    testing herouku-build.js

---
+++ ops/heroku-build.js
@@ -0,0 +1,23 @@
+'use strict';
+
+if ('HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/app')){
+
+  const ChildProcess = require('child_process');
+
+  try {
+    console.time("install");
+    console.log("starting npm install of dev dependencies");
+    ChildProcess.execSync(`pip install selenium`);
+    console.timeEnd("install");
+
+    console.time("build");
+    console.log("starting npm build");
+    ChildProcess.execSync(`npm run build:all`);
+    console.timeEnd("build");
+  }
+  catch (err) {
+    console.error(err.message);
+  }
+} else {
+  console.log("Not Heroku, skipping postinstall build");
+}
