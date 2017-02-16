3f41473bb1783eb03bf1c0d3a4f4378cc060eb3e
sachdevnitin19 <sachdevnitin19@gmail.com>
2017-02-16 21:03:56 +0530

    testing 2

--- ops/heroku-build.js
+++ ops/heroku-build.js
@@ -7,13 +7,13 @@ if ('HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/
   try {
     console.time("install");
     console.log("starting npm install of dev dependencies");
-    ChildProcess.execSync(`python --version`);
+    ChildProcess.execSync(`python3 --version`);
     console.timeEnd("install");
 
-    console.time("build");
+    /*console.time("build");
     console.log("starting npm build");
     ChildProcess.execSync(`npm run build:all`);
-    console.timeEnd("build");
+    console.timeEnd("build");*/
   }
   catch (err) {
     console.error(err.message);
