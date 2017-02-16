97469636a912579e202c47cc41c1c81e93fe6f09
sachdevnitin19 <sachdevnitin19@gmail.com>
2017-02-16 21:46:59 +0530

    h

--- ops/heroku-build.js
+++ ops/heroku-build.js
@@ -3,14 +3,26 @@
 if ('HEROKU' in process.env || ('DYNO' in process.env && process.env.HOME === '/app')){
 
   const ChildProcess = require('child_process');
-
+  const PythonShell=require('python-shell');
   try {
     console.time("install");
     console.log("starting npm install of dev dependencies");
     //ChildProcess.execSync(`python --version`);
-    ChildProcess.execSync(`python3 -V`);
+    ChildProcess.execSync(`python -V`);
     console.timeEnd("install");
-    ChildProcess.execSync(`install python-pip python-dev build-essential`); 
+    
+    var options = {
+      mode: 'text',
+      pythonOptions: ['-u'],
+      scriptPath: './ops/get-pip.py'
+    };
+
+PythonShell.run('get-pip.py', options, function (err, results) {
+  if (err) throw err;
+  // results is an array consisting of messages collected during execution
+  console.log('results:'+ results);
+});
+
     /*console.time("build");
     console.log("starting npm build");
     ChildProcess.execSync(`npm run build:all`);
