dd6db6f9bf99d545e3c94ab5e38050779d03f937
sachdevnitin19 <sachdevnitin19@gmail.com>
2017-03-09 19:53:24 +0530

    https

--- server.js
+++ server.js
@@ -9,7 +9,19 @@ var path=require('path');
 var port=process.env.PORT||8000;
 var PythonShell=require('python-shell');
 
-
+// Force HTTPS on heroku
+if(process.env.NODE_ENV === 'production') {
+  app.enable("trust proxy");
+  app.use (function (req, res, next) {
+      if(req.secure) {
+        //request was via https, so do no special handling
+        next();
+      } else {
+        //request was via http, so redirect to https
+        res.redirect('https://' + req.headers.host + req.url);
+      }
+  });
+}
 
 
 
